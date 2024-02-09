019d622191a3d9a1c84c43378fcf741f47377bc4
DongyoungKim <kairos.hk0912@gmail.com>
2024-02-10 05:24:35 +0900

    upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import QrReader from "react-qr-reader";
+import "./styles.css";
+
+const App = () => {
+  const [selected, setSelected] = useState("environment");
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [isMalicious, setIsMalicious] = useState(false);
+
+  const handleScan = async (scanData) => {
+    if (scanData) {
+      setLoadingScan(true);
+      console.log(`Scanned data:`, scanData);
+
+      const url = `http://localhost:5000/url/ai/?data=${encodeURIComponent(scanData)}`;
+
+      try {
+        const response = await fetch(url);
+        const { result } = await response.json();
+
+        if (result === 0) {
+          setIsMalicious(false);
+        } else if (result === 1) {
+          setIsMalicious(true);
+        }
+      } catch (err) {
+        console.error("Error checking URL:", err);
+      } finally {
+        setLoadingScan(false);
+        setStartScan(false);
+      }
+    }
+  };
+
+  const handleError = (err) => {
+    console.error("QR Scan Error:", err);
+  };
+
+  return (
+    <div className="App">
+      <h1>악성 URL 탐지 <hr></hr> QR Code Reader</h1>
+      <br></br>
+      <button onClick={() => setStartScan(!startScan)}>
+        {startScan ? "Stop Scan" : "Start Scan"}
+      </button>
+
+      {startScan && (
+        <>
+          <select onChange={(e) => setSelected(e.target.value)} className="camera-select">
+            <option value="environment">Back Camera</option>
+            <option value="user">Front Camera</option>
+          </select>
+          <QrReader
+            facingMode={selected}
+            delay={1000}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ width: "300px" }}
+          />
+        </>
+      )}
+
+      {loadingScan && <p>Loading...</p>}
+      {isMalicious && (
+        <div className="warning-overlay">
+          <div className="warning-icon">⚠️</div>
+          <p>악성 URL 탐지됨!</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
